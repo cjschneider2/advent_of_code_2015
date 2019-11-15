9c91cb07f2512546e85df25574a33a1f1d4705f6
csjchneider2 <cjschneider2@gmail.com>
2019-11-15 23:04:54 +0100

    solved day 3 part 2

--- day_3/day_3.exs
+++ day_3/day_3.exs
@@ -25,7 +25,50 @@ defmodule AOC2015.Day1 do
     IO.puts("part 1 : " <> Integer.to_string(p1(to_charlist(steps))))
   end
 
+
+  defmodule Count do
+    defstruct [s: 0, r: 0]
+  end
+
+  defp p2(steps) do p2(steps, %{}, {0, 0}, {0, 0}, %Count{}) end
+  defp p2([], _h, _s_p, _r_p, c = %Count{}) do
+    c.s + c.r
+  end
+  defp p2([s_dir | [r_dir | tail]], h, s_p = {sx, sy}, r_p = {rx, ry}, cnt) do
+    next = Map.update(h, s_p, 1, fn c -> c + 1 end)
+    s_u = if Map.fetch!(next, s_p) == 1 do 1 else 0 end
+    next = Map.update(next, r_p, 1, fn c -> c + 1 end)
+    r_u = if Map.fetch!(next, r_p) == 1 do 1 else 0 end
+    s_pos = case s_dir do
+      ?> -> {sx + 1, sy}
+      ?^ -> {sx, sy + 1}
+      ?< -> {sx - 1, sy}
+      ?v -> {sx, sy - 1}
+    end
+    r_pos = case r_dir do
+      ?> -> {rx + 1, ry}
+      ?^ -> {rx, ry + 1}
+      ?< -> {rx - 1, ry}
+      ?v -> {rx, ry - 1}
+    end
+    case r_dir do
+      ?> -> p2(tail, next, s_pos, r_pos, %Count{cnt | s: cnt.s + s_u, r: cnt.r + r_u} )
+      ?^ -> p2(tail, next, s_pos, r_pos, %Count{cnt | s: cnt.s + s_u, r: cnt.r + r_u} )
+      ?< -> p2(tail, next, s_pos, r_pos, %Count{cnt | s: cnt.s + s_u, r: cnt.r + r_u} )
+      ?v -> p2(tail, next, s_pos, r_pos, %Count{cnt | s: cnt.s + s_u, r: cnt.r + r_u} )
+    end
+  end
+
+  @doc """
+  Here, we need to find how many unique houses both Santa and
+  Robo-Santa have visited
+  """
+  def run_part2() do
+    {:ok, steps} = File.read("day-3_test-input.txt")
+    IO.puts("part 2 : " <> Integer.to_string(p2(to_charlist(steps))))
+  end
+
 end
 
 AOC2015.Day1.run_part1()
-#AOC2015.Day1.run_part2()
+AOC2015.Day1.run_part2()
