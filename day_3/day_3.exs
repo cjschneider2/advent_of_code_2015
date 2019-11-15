2a80d68931d8f4d9e3d2f9f015e62f11dab2cbbd
csjchneider2 <cjschneider2@gmail.com>
2019-11-15 22:38:25 +0100

    solve day 3 part 1

--- day_3/day_3.exs
+++ day_3/day_3.exs
@@ -1,11 +1,19 @@
 defmodule AOC2015.Day1 do
 
-  defp p1(steps) do p1(steps, 0) end
-  defp p1([], floor) do floor end
-  defp p1([head | tail], floor) do
+  defp p1(steps) do p1(steps, %{}, {0, 0}, 0) end
+  defp p1([], _houses, _pos, count) do count end
+  defp p1([head | tail], houses, {x, y}, count) do
+    cnt = case Map.fetch(houses, pos = {x, y}) do
+      {:ok, cnt} -> cnt
+      :error -> 0
+    end
+    uni = if cnt == 0 do 1 else 0 end
+    next = Map.update(houses, pos, 1, fn c -> c + 1 end)
     case head do
-      ?) -> p1(tail, floor - 1)
-      ?( -> p1(tail, floor + 1)
+      ?> -> p1(tail, next, {x + 1, y}, count + uni)
+      ?^ -> p1(tail, next, {x, y + 1}, count + uni)
+      ?< -> p1(tail, next, {x - 1, y}, count + uni)
+      ?v -> p1(tail, next, {x, y - 1}, count + uni)
     end
   end
 
@@ -13,7 +21,7 @@ defmodule AOC2015.Day1 do
   Here, we need to find how many unique houses Santa has visited
   """
   def run_part1() do
-    {:ok, steps} = File.read("day-1_test-input.txt")
+    {:ok, steps} = File.read("day-3_test-input.txt")
     IO.puts("part 1 : " <> Integer.to_string(p1(to_charlist(steps))))
   end
 
